afe15a547854237cc1383c33fd6994ae7734e932
Stephen Odundo <steveremdi@gmail.com>
2021-05-28 23:28:44 +0300

    first attempt

--- SmallestSubArrayofGivenSum/Program.cs
+++ SmallestSubArrayofGivenSum/Program.cs
@@ -6,7 +6,23 @@ namespace SmallestSubArrayofGivenSum
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var subArray = SmallestSubArray(new int[] {2, 1, 5, 2, 3, 2}, 7);
+            Console.WriteLine(subArray);
+        }
+
+        private static int SmallestSubArray(int[] array, int S)
+        {
+            int windowStart = 0, subArray = 0, windowSum = 0;
+
+            for (int windowEnd = 0; windowEnd < array.Length; windowEnd++)
+            {
+                windowSum += array[windowEnd];
+                subArray++;
+                
+                if(windowSum >= S)
+                    return subArray;
+            }
+            return 0;
         }
     }
 }
