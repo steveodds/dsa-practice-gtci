b005b203cf68b5374c8ca723f3a77657e2a80e46
Stephen Odundo <steveremdi@gmail.com>
2021-05-28 23:44:05 +0300

    solution

--- SmallestSubArrayofGivenSum/Program.cs
+++ SmallestSubArrayofGivenSum/Program.cs
@@ -6,23 +6,28 @@ namespace SmallestSubArrayofGivenSum
     {
         static void Main(string[] args)
         {
-            var subArray = SmallestSubArray(new int[] {2, 1, 5, 2, 3, 2}, 7);
+            var subArray = SmallestSubArray(new int[] {3, 4, 1, 1, 6}, 8);
             Console.WriteLine(subArray);
         }
 
         private static int SmallestSubArray(int[] array, int S)
         {
-            int windowStart = 0, subArray = 0, windowSum = 0;
+            //Start by increasing the windowsum to meet the required criteria
+            int windowStart = 0, subArray = Int32.MaxValue, windowSum = 0;
 
             for (int windowEnd = 0; windowEnd < array.Length; windowEnd++)
             {
-                windowSum += array[windowEnd];
-                subArray++;
+                windowSum += array[windowEnd]; //increase windowsum
                 
-                if(windowSum >= S)
-                    return subArray;
+                while (windowSum >= S) //drop in here when windowsum meets threshold
+                {
+                    //check if subarray(min length) is the smallest otherwise get smallest from window tracker
+                    subArray = subArray < (windowEnd - windowStart + 1) ? subArray : windowEnd - windowStart + 1;
+                    windowSum -= array[windowStart]; //reduce windowsize, will drop out of while when window is too small
+                    windowStart++; //shift window
+                }
             }
-            return 0;
+            return subArray == -1 ? 0 : subArray; //return 0 if no minimum was found or the actual min
         }
     }
 }
